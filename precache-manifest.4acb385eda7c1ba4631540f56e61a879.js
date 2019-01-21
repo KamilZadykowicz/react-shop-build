eca3455b8232a6ac9b17a1ec4558c7c578a913cb
KamilZadykowicz <zadykowicz14@gmail.com>
2019-01-22 00:04:37 +0100

    Add files via upload

---
+++ precache-manifest.4acb385eda7c1ba4631540f56e61a879.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "80fd1838f22ca85c46da5c2dbb719f44",
+    "url": "/static/media/7.80fd1838.jpg"
+  },
+  {
+    "revision": "9702e2a986cae98c3ee8",
+    "url": "/static/css/main.e1e39bf1.chunk.css"
+  },
+  {
+    "revision": "100dd595e83337665d49",
+    "url": "/static/js/1.100dd595.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "dfe9329c42313c9ce70ad72b9e38185e",
+    "url": "/static/media/5.dfe9329c.jpg"
+  },
+  {
+    "revision": "84310b191cf7090317acb989879baf12",
+    "url": "/static/media/6.84310b19.jpg"
+  },
+  {
+    "revision": "9702e2a986cae98c3ee8",
+    "url": "/static/js/main.9702e2a9.chunk.js"
+  },
+  {
+    "revision": "80195a1dabdf794955af7974b4971802",
+    "url": "/static/media/8.80195a1d.jpg"
+  },
+  {
+    "revision": "9626e205a383130b595870c31f382046",
+    "url": "/static/media/9.9626e205.jpg"
+  },
+  {
+    "revision": "b7365e8f29d8621ed46f13f323093eeb",
+    "url": "/static/media/shopping-cart.b7365e8f.svg"
+  },
+  {
+    "revision": "dce2cafa140d981289aeea3fee59b2d2",
+    "url": "/static/media/bin.dce2cafa.svg"
+  },
+  {
+    "revision": "ff96f5365af1d362157fd850e08b33e1",
+    "url": "/index.html"
+  }
+];
